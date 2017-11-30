23f53ec53f5f0d74c64540b7119db115d16be074
neilsatt <neilsatt@gmail.com>
2017-11-29 21:19:26 -0700

    update folder structure

---
+++ Basics/2_ThinkingInComponents/app_version2_decomposing/app.jsx
@@ -0,0 +1,50 @@
+function Application(props) {
+  return (
+      <div className="scoreboard">
+        <div className="header">
+            <h1>{props.title}</h1>  
+        </div>
+          
+         <div className="players">
+            <div className="player">
+                <div className="player-name">
+                   Bilasion Stratmon
+                </div>
+                <div className="player-score">
+                    <div className="counter">
+                        <button className="counter-action decrement"> - </button>
+                        <div className="counter-score"> 31 </div>
+                        <button className="counter-action increment"> + </button>
+                    </div>         
+                </div>
+            </div>
+            
+            <div className="player">
+                <div className="player-name">
+                   Riley Godren
+                </div>
+                <div className="player-score">
+                    <div className="counter">
+                        <button className="counter-action decrement"> - </button>
+                        <div className="counter-score"> 44 </div>
+                        <button className="counter-action increment"> + </button>
+                    </div>         
+                </div>
+             </div>
+        </div>
+      </div>
+  ); 
+}
+
+Application.propTypes = {
+    title: React.PropTypes.string,
+};
+
+Application.defaultProps = {
+    title: "Scoreboard",
+}
+
+ReactDOM.render(
+    <Application />, document.getElementById('container')
+);
+
