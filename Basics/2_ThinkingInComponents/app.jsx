bbb54491477cb063963838091580e3c1f92d4a57
neilsatt <neilsatt@gmail.com>
2017-11-29 18:55:26 -0700

    add project and rearrange folders

---
+++ Basics/2_ThinkingInComponents/app.jsx
@@ -0,0 +1,42 @@
+function Application() {
+  return (
+      <div className="scoreboard">
+        <div className="header">
+            <h1>Scoreboard</h1>  
+        </div>
+          
+        <div className="players">
+          <div className="player">
+            <div className="player-name">
+               Bilasion Stratmon
+            </div>
+            <div className="player-score">
+                <div className="counter">
+                    <button className="counter-action decrement"> - </button>
+                    <div className="counter-score"> 31 </div>
+                    <button className="counter-action increment"> + </button>
+                </div>         
+            </div>
+         </div>
+            
+        <div className="player">
+            <div className="player-name">
+               Riley Godren
+            </div>
+            <div className="player-score">
+                <div className="counter">
+                    <button className="counter-action decrement"> - </button>
+                    <div className="counter-score"> 44 </div>
+                    <button className="counter-action increment"> + </button>
+                </div>         
+            </div>
+         </div>
+        </div>
+      </div>
+  ); 
+}
+
+ReactDOM.render(
+    <Application />, document.getElementById('container')
+);
+
