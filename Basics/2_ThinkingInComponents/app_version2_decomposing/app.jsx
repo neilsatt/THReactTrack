44bb8ef74f2000421c1660721fff31e5b2cea93b
neilsatt <neilsatt@gmail.com>
2017-12-02 17:43:50 -0700

    Decomposing section

--- Basics/2_ThinkingInComponents/app_version2_decomposing/app.jsx
+++ Basics/2_ThinkingInComponents/app_version2_decomposing/app.jsx
@@ -1,37 +1,68 @@
-function Application(props) {
-  return (
-      <div className="scoreboard">
+/* Our goal is to move each of the sections into their own components
+
+    - Header
+    - Player
+    - 
+    
+*/
+
+function Header(props) {
+    return(
         <div className="header">
             <h1>{props.title}</h1>  
         </div>
+    );
+}
+
+Header.propTypes = {
+    title: React.PropTypes.string.isRequired,
+};
+
+function Counter(props) {
+    return(
+            <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> {props.score} </div>
+                <button className="counter-action increment"> + </button>
+            </div>  
+    );
+};
+
+Counter.propTypes = {
+    score: React.PropTypes.number.isRequired,
+};
+
+
+
+function Player(props){
+    return (
+        <div className="player">
+                <div className="player-name">
+                   {props.name}
+                </div>
+                <div className="player-score">
+                    <Counter score={props.score} />
+                </div>
+        </div>
+    )
+}
+
+Player.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired,
+};
+
+function Application(props) {
+  return (
+      <div className="scoreboard">
+     
+        <Header title={props.title}/> 
+          
           
          <div className="players">
-            <div className="player">
-                <div className="player-name">
-                   Bilasion Stratmon
-                </div>
-                <div className="player-score">
-                    <div className="counter">
-                        <button className="counter-action decrement"> - </button>
-                        <div className="counter-score"> 31 </div>
-                        <button className="counter-action increment"> + </button>
-                    </div>         
-                </div>
-            </div>
-            
-            <div className="player">
-                <div className="player-name">
-                   Riley Godren
-                </div>
-                <div className="player-score">
-                    <div className="counter">
-                        <button className="counter-action decrement"> - </button>
-                        <div className="counter-score"> 44 </div>
-                        <button className="counter-action increment"> + </button>
-                    </div>         
-                </div>
-             </div>
-        </div>
+             <Player name="Jim Saylor" score={23}/> 
+             <Player name="Riley Godren" score={44}/> 
+         </div>
       </div>
   ); 
 }
