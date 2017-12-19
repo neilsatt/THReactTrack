184fe34545c98389c644ba5c22172ac79cc02c67
neilsatt <neilsatt@gmail.com>
2017-12-18 20:08:58 -0700

    update src folder content

--- BuildingAppsWithReactAndRedux/app/index.js
+++ BuildingAppsWithReactAndRedux/app/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import PlayerReducer from './src/reducers/player';
 import Scoreboard from './src/containers/Scoreboard';
 
+/*
+  In order to provide a redux store to our application,
+  we'll create a store that's composed of our player reducer and
+  pass that store to the reactor redux provider component.
+*/
+
+const store = createStore(
+ PlayerReducer
+);
+
+/*
+ Wrapping our application with the provider
+ component we've created a method for any container component
+ to subscribe to Redux data changes, which is essential for passing
+ data down to other components.
+*/
+
 render(
- <Scoreboard />,
+  <Provider store={store}>
+    <Scoreboard />
+  </Provider>,
  document.getElementById('root')
 );
