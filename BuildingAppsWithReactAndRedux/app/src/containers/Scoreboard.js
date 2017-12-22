a8efc67a270d9d3178eaa0469c2545c6e1cd7bde
neilsatt <neilsatt@gmail.com>
2017-12-21 20:25:16 -0700

    update section 4

--- BuildingAppsWithReactAndRedux/app/src/containers/Scoreboard.js
+++ BuildingAppsWithReactAndRedux/app/src/containers/Scoreboard.js
@@ -1,24 +1,19 @@
-import React, {Component, PropTypes} from 'react';
-import { bindActionCreators} from 'redux';
-import {connect } from 'react-redux';
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import * as PlayerActionCreators from '../actions/player';
-import Stopwatch from '../components/Stopwatch';
-import Counter from '../components/Counter';
-import Stats from '../components/Stats';
-import AddPlayerForm from '../components/AddPlayerForm';
 import Player from '../components/Player';
 import Header from '../components/Header';
+import AddPlayerForm from '../components/AddPlayerForm';
 
+class Scoreboard extends Component {
 
+  static propTypes = {
+    players: PropTypes.array.isRequired
+  };
 
-class Scoreboard extends Component{
-   static propTypes = {
-      players: PropTypes.array.isRequired
-   };
-
-  render () {
-
-    const {dispatch, players } = this.props;
+  render() {
+    const { dispatch, players } = this.props;
     const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispatch);
     const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispatch);
     const updatePlayerScore = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispatch);
@@ -27,23 +22,24 @@ class Scoreboard extends Component{
       <Player
         index={index}
         name={player.name}
-        score={player.switch}
+        score={player.score}
         key={player.name}
         updatePlayerScore={updatePlayerScore}
         removePlayer={removePlayer}
       />
-    ))
+    ));
+
     return (
       <div className="scoreboard">
         <Header players={players} />
         <div className="players">
-            {playerComponents}
+          { playerComponents }
         </div>
         <AddPlayerForm addPlayer={addPlayer} />
       </div>
     );
   }
-};
+}
 
 const mapStateToProps = state => (
   {
@@ -51,11 +47,4 @@ const mapStateToProps = state => (
   }
 );
 
-/* Subscribe Scoreboard to any changes in state or any Redux store updates
-   results passed as a prop to scoreboard.
-   Any time our underlying player state changes,
-   it will be mapped to a property called players.
-   That is injected into our Scoreboard container making it
-   available to any downstream components.
- */
 export default connect(mapStateToProps)(Scoreboard);
