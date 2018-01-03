e61df243ca56c4c8583f0205e9e5dbbca190c808
neilsatt <neilsatt@gmail.com>
2018-01-02 20:58:07 -0700

    add counter and pending guest

--- ReactByExample/App/rsvp/src/App.js
+++ ReactByExample/App/rsvp/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import GuestList from './GuestList';
+import Counter from './Counter';
 
 class App extends Component {
 
@@ -42,6 +44,14 @@ class App extends Component {
   toggleConfirmationAt = index =>
     this.toggleGuestPropertyAt("isConfirmed", index);
 
+  removeGuestAt = index =>
+    this.setState({
+      guests: [
+        ...this.state.guests.slice(0, index),
+        ...this.state.guests.slice(index + 1)
+      ]
+    });
+
   toggleEditingAt = index =>
     this.toggleGuestPropertyAt("isEditing", index);
 
@@ -80,10 +90,18 @@ class App extends Component {
   }
 
   getTotalInvited = () => this.state.guests.length;
-  // getAttendingGuests = () =>
-  // getUnconfirmedGuests = () =>
+
+  getAttendingGuests = () =>
+    this.state.guests.reduce(
+      (total, guest) => guest.isConfirmed ? total + 1 : total,
+      0
+    );
 
   render() {
+    const totalInvited = this.getTotalInvited();
+    const numberAttending = this.getAttendingGuests();
+    const numberUnconfirmed = totalInvited - numberAttending;
+
     return (
       <div className="App">
         <header>
@@ -108,22 +126,11 @@ class App extends Component {
                 checked={this.state.isFiltered} /> Hide those who haven't responded
             </label>
           </div>
-          <table className="counter">
-            <tbody>
-              <tr>
-                <td>Attending:</td>
-                <td>2</td>
-              </tr>
-              <tr>
-                <td>Unconfirmed:</td>
-                <td>1</td>
-              </tr>
-              <tr>
-                <td>Total:</td>
-                <td>3</td>
-              </tr>
-            </tbody>
-          </table>
+
+          <Counter
+            totalInvited={totalInvited}
+            numberAttending={numberAttending}
+            numberUnconfirmed={numberUnconfirmed} />
 
           <GuestList
             guests={this.state.guests}
@@ -131,6 +138,8 @@ class App extends Component {
             toggleEditingAt={this.toggleEditingAt}
             setNameAt={this.setNameAt}
             isFiltered={this.state.isFiltered}
+            removeGuestAt={this.removeGuestAt}
+            pendingGuest={this.state.pendingGuest}
           />
 
         </div>
